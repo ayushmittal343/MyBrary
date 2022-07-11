415cc7579667f6ba1f7712f7bd343fd6607000e9
ayush343 <ayushmittal343@gmail.com>
2022-07-11 13:36:51 +0530

    initial author routes

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const express= require('express')
 const app=express();
 const expressLayouts= require('express-ejs-layouts')
 const mongoose=require('mongoose')
+const bodyParser=require("body-parser")
+
+
 const indexRouter=require("./routes/index")
+const authorRouter=require("./routes/authors")
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views');
@@ -18,6 +22,7 @@ app.set('layout','layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb',extended:true}))
 
 const DATABASE_URL="mongodb+srv://ayushmittal:ayush123@cluster0.zka7z.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(DATABASE_URL,{useNewUrlParser: true})
@@ -29,5 +34,7 @@ db.once('open',()=>console.log('Connected to Mongoose'))
 
 
 app.use("/",indexRouter)
+app.use("/authors",authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
