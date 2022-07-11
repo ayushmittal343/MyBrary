ab48dfc77d0b9b17d4b818e2dad09af3804b5846
ayush343 <ayushmittal343@gmail.com>
2022-07-11 11:30:48 +0530

    changed database url

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-require('dotenv').config()
+// require('dotenv').config()
+// if(procees.env.NODE_ENV!=='production'){
+//     require('dotenv').parse()
+// }
+// if (process.env.NODE_ENV !== 'production') {
+//     require('dotenv').parse()
+//   }
 const express= require('express')
 const app=express();
 const expressLayouts= require('express-ejs-layouts')
@@ -9,15 +15,19 @@ app.set('view engine','ejs')
 app.set('views',__dirname+'/views');
 app.set('layout','layouts/layout')
 
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
+
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const DATABASE_URL="mongodb+srv://ayushmittal:ayush123@cluster0.zka7z.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(DATABASE_URL,{useNewUrlParser: true})
 
 const db=mongoose.connection
 
 db.on('error',error => console.error(error))
 db.once('open',()=>console.log('Connected to Mongoose'))
 
-app.use(expressLayouts)
-app.use(express.static('public'))
+
 app.use("/",indexRouter)
 
 app.listen(process.env.PORT || 3000)
